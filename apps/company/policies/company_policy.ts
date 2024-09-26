d76600c80ddc35bd2dee0aba5688918bd6c163fc
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-09-26 14:42:17 +0200

    feat: Add company index route with authorization and validation
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

---
+++ apps/company/policies/company_policy.ts
@@ -0,0 +1,38 @@
+import { AuthorizationResponse, BasePolicy } from '@adonisjs/bouncer'
+import RoleService from '#apps/shared/services/role_service'
+import { inject } from '@adonisjs/core'
+import { JWTPayload } from '#apps/authentication/contracts/jwt'
+import { Roles } from '#apps/shared/contracts/roles'
+import CompanyException from '#apps/company/exceptions/company_exception'
+
+@inject()
+export default class CompanyPolicy extends BasePolicy {
+  constructor(protected roleService: RoleService) {
+    super()
+  }
+
+  async after(_payload: JWTPayload, _action: string, response: AuthorizationResponse) {
+    if (!response.authorized) {
+      throw new CompanyException(response.message, {
+        status: response.status,
+        code: 'E_COMPANY_FORBIDDEN',
+      })
+    }
+  }
+
+  async view(payload: JWTPayload) {
+    if (this.roleService.verifyAccess(payload, Roles.VIEW_COMPANY)) {
+      return AuthorizationResponse.allow()
+    }
+
+    return AuthorizationResponse.deny('You are not authorized to view companies', 403)
+  }
+
+  async create(payload: JWTPayload) {
+    if (this.roleService.verifyAccess(payload, Roles.CREATE_COMPANY)) {
+      return AuthorizationResponse.allow()
+    }
+
+    return AuthorizationResponse.deny('You are not authorized to create companies', 403)
+  }
+}
--- apps/company/validators/company.ts
+++ apps/company/validators/company.ts
@@ -5,7 +5,26 @@ import { Infer } from '@vinejs/vine/types'
  * Validator to validate the payload when creating
  * a new company.ts.
  */
-export const createCompanyValidator = vine.compile(vine.object({}))
+export const createCompanyValidator = vine.compile(
+  vine.object({
+    name: vine.string(),
+    siret: vine.string(),
+    approvalNumber: vine.string(),
+    address: vine.object({
+      street: vine.string(),
+      postalCode: vine.string(),
+      city: vine.string(),
+      region: vine.string(),
+      country: vine.string(),
+    }),
+    contact: vine.object({
+      email: vine.string(),
+      mobilePhone: vine.string().optional(),
+      landlinePhone: vine.string().optional(),
+      website: vine.string().optional(),
+    }),
+  })
+)
 
 /**
  * Validator to validate the payload when updating
@@ -13,5 +32,13 @@ export const createCompanyValidator = vine.compile(vine.object({}))
  */
 export const updateCompanyValidator = vine.compile(vine.object({}))
 
+export const getCompaniesValidator = vine.compile(
+  vine.object({
+    page: vine.number().optional(),
+    limit: vine.number().optional(),
+  })
+)
+
 export type CreateCompanySchema = Infer<typeof createCompanyValidator>
 export type UpdateCompanySchema = Infer<typeof updateCompanyValidator>
+export type GetCompaniesSchema = Infer<typeof getCompaniesValidator>
