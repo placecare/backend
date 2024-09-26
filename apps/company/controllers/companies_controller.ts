47d8d61e89db18143e8da7cd885bcb01423980e0
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-09-26 14:42:04 +0200

    feat: Add company index route with authorization and validation
    The code changes in this commit add an index route for companies. It includes authorization using the `bouncer` middleware and validation using the `getCompaniesValidator`. This allows only authorized users to view the list of companies and ensures that the request parameters are validated before processing.
    
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- apps/company/controllers/companies_controller.ts
+++ apps/company/controllers/companies_controller.ts
@@ -1,25 +1,46 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { inject } from '@adonisjs/core'
-//import CompanyService from '#apps/company/services/company_service'
+import CompanyService from '#apps/company/services/company_service'
+import { createCompanyValidator, getCompaniesValidator } from '#apps/company/validators/company'
+import CompanyPolicy from '#apps/company/policies/company_policy'
 
 @inject()
 export default class CompaniesController {
-  //constructor(private companyService: CompanyService) {}
+  constructor(private companyService: CompanyService) {}
 
   /**
-   * Display a list of resource
+   * @index
+   * @operationId getCompanies
+   * @description Returns array of companies
+   * @responseBody 200 - <Company[]>.paginated()
+   * @paramUse(sortable, filterable)
+   * @responseHeader 200 - @use(paginated)
+   * @responseHeader 200 - X-pages - A description of the header - @example(test)
    */
-  async index({}: HttpContext) {}
-
-  /**
-   * Handle form submission for the create action
-   */
-  async store({}: HttpContext) {}
+  async index({ request, bouncer }: HttpContext) {
+    await bouncer.with(CompanyPolicy).authorize('view' as never)
+    const data = await request.validateUsing(getCompaniesValidator)
+    return this.companyService.findAll(data)
+  }
 
   /**
    * Show individual record
    */
-  async show({}: HttpContext) {}
+  async show({ params }: HttpContext) {
+    const { id } = params
+    return this.companyService.findById(id)
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, bouncer }: HttpContext) {
+    await bouncer.with(CompanyPolicy).authorize('create' as never)
+    const data = await request.validateUsing(createCompanyValidator)
+
+    return data
+    //return this.companyService.create(data)
+  }
 
   /**
    * Handle form submission for the edit action
